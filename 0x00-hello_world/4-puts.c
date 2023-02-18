38c625019c6fcf19371688d326e7bfeab62661d0
kurves <kavssimply@gmail.com>
2023-02-18 08:25:25 -0800

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
