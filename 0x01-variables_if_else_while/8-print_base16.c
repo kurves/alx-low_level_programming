cbb9faccc3d16c94d1cc53b2a71152463345fefb
kurves <kavssimply@gmail.com>
2023-02-19 06:03:19 -0800

    print hexadecimal values

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	char af;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (af = 'a'; af <= 'f'; af++)
+		putchar(af);
+	}
+	putchar('\n');
+	return (0);
+}
