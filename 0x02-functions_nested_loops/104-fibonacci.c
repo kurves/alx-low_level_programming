ca5c5e82bd4692cd03667705be28eb1aa6577aa9
kurves <kavssimply@gmail.com>
2023-02-22 10:32:00 -0800

    first 98 fibonucci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
-	for (i = 2; i < 50; i++)
+	for (i = 2; i < 98; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if (i == 97)
