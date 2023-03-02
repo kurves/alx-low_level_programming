eb3eb3f18b5aace9528e8b849371781cf274593f
kurves <kavssimply@gmail.com>
2023-03-02 05:23:17 -0800

    Concantenate Strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _strcat - Concatenates two strings
+  * @dest: The destination string
+  * @src: The source string
+  *
+  * Return: A pointer to the resulting string dest
+  */
+char *_strcat(char *dest, char *src)
+{
+	int destlen = 0, i;
+
+	while (dest[destlen])
+	{
+		destlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[destlen] = src[i];
+		dlen++;
+	}
+
+	dest[destlen] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Concantenate strings
