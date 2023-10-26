368ba3367626125bfc9efb8b8d0746033c10769c
kurves <kavssimply@gmail.com>
2023-10-26 13:12:24 -0700

    Betty coding style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,13 +6,14 @@
  *  _strlen_recursion - a function that printsthe length of a string
  * @s: characterstring
  * Description: A function that prints the length of a string
+ * Return: Len of string
  */
 int _strlen_recursion(char *s)
 {
 
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
-	return 1 +  _strlen_recursion(s + 1);
+	return (1 +  _strlen_recursion(s + 1));
 }
