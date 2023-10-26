5de8406f683840213ced85cba2b597dcee215603
kurves <kavssimply@gmail.com>
2023-10-26 12:05:46 -0700

    Betty coding style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,18 @@
 #include <stdio.h>
 
 /**
- * void _puts_recursion - a function that prints a string
+ * _puts_recursion - a function that prints a string
+ * @s: characterstring
+ * Description: A function that writes a string recursively
  */
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
 		putchar('\n');
-		return; 
+		return;
 	
 	}
 	putchar(*s);
-	_puts_recursion(s+1);
+	_puts_recursion(s + 1);
 }	
