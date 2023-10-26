d780339c06c3bac7d4bb25946935d2cb8ecbe6a6
kurves <kavssimply@gmail.com>
2023-10-26 13:26:25 -0700

    Fucntion to return factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include  "main.h"
+#include <stdio.h>
+i
+/**
+ * factorial(int n) - a function that prints the factorial of a number
+ * @n: integer
+ * Description: A function that prints the factorial of a number recursively
+ * Return: Integer value
+ */
+int factorial(int n);
+{
+
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	elif (n == -1)
+	{
+		return (-1);
+	}
+	return (n * factorial(n-1);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,3 +1,4 @@
 Function that prints a string recursively
 Function that prints a string in reverse
 function to prin the lengt of a string
+Factorial function
