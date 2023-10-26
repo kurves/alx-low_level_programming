1438be4670dae3b50a21a3a56745582abbe0dd06
kurves <kavssimply@gmail.com>
2023-10-26 11:07:57 -0700

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include  main.h
+#include stdio.h
+
+/**
+ * void _puts_recursion- a function that prints a string
+ */
+void _puts_recursion(char,*s)
+{
+	if(*s == 0){
+		putchar('\n');
+		return; 
+	
+	}
+	putchar(*s);
+	_puts_recursion(s+1);
+}	
