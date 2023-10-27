02c618b1b6be33a627a1ce27ad7baa1e99d8417b
kurves <kavssimply@gmail.com>
2023-10-27 09:40:23 -0700

    program to print the args passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Program to print the arguments passed
+ * @argc: argument count
+ * @argv: array of arguments
+ * Description: A program to print the arguments passed
+ * Return: integer
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
