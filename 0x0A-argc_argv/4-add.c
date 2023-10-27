559f1c5089d670eeabfae2e4ea564f9dc3396f46
kurves <kavssimply@gmail.com>
2023-10-27 14:00:04 -0700

    Program to add to integers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,19 +13,18 @@ int main(int argc, char **argv)
 	int i, sum = 0;
 	if (argc == 1)
 	{
-		printf("0\n");
-        	return (1);
-        }
+		  printf("0\n");
+	}
 	for (i = 1; i < argc; i++)
-       	{
+	{
 		for (int j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
-                       	{
-                        	printf("Error\n");
-	                        return (1);
-        	        }
-        	}
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
