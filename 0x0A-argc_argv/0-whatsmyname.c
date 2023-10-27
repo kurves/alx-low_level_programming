a983faf500091d3e6e26757760b7a30bbced5173
kurves <kavssimply@gmail.com>
2023-10-27 08:20:34 -0700

    Function that prints the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Program to print the program name
+ * @argc: argument count
+ * @argv: array of arguments
+ * Description: A program to print theprogram name
+ * Return: integer
+ */
+
+int main(int argc, char **argv)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
