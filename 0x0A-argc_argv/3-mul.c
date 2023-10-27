63769cf707623c02084899d84665e86d3230b576
kurves <kavssimply@gmail.com>
2023-10-27 14:31:34 -0700

    Program to multiply integers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,19 +13,11 @@ int main(int argc, char **argv)
 	int i, j, mul = 1;
 	if (argc == 1)
 	{
-		  printf("0\n");
-		  return (0);
+		  printf("Error\n");
+		  return (1);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
 		mul *= atoi(argv[i]);
 	}
 	printf("%d\n", mul);
