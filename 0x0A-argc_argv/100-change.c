9c85a4d40b1e4d00aa4231a0004e10826e811764
kurves <kavssimply@gmail.com>
2023-10-28 10:59:57 -0700

    Program that prints the minimum number of coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Program that returns minmal number of coins
+ * @argc: argument count
+ * @argv: array of arguments
+ * Description: A program that returns minimal number of coins to return change
+ * Return: integer
+ */
+int main(int argc, char **argv)
+{
+	int i,  coins[] = {25, 10, 5, 2}, no_of_coins = 0, total = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	int amount = atoi(argv[1]);
+
+	while (amount > 1)
+	{
+		for (i = 0; i < sizeof(coins[i]); i++)
+		{
+			if (amount >= coins[i])
+			{
+				int cents = amount / coins[i];
+				
+				no_of_coins +=  cents;
+				total = amount % coins[i];
+				amount = total;
+			}
+		}
+	}
+	if (amount == 1)
+	{
+		no_of_coins++;
+	}
+	printf("%d\n", no_of_coins);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 Argc and Argv
 Function to return the number of arguments ppassed
+Minimal number of coins for change
