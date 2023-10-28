8675eac4f949c94982d67b223e3afb8057f94f1d
kurves <kavssimply@gmail.com>
2023-10-28 12:51:24 -0700

    Fix compilation errors

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,11 +12,12 @@ int is_palindrome(char *s)
 
 	if (strlen(s) == 0)
 	{
-		return (0);
+		return (1);
 	}
 	if (s[0] != s[strlen(s) - 1])
 	{
-		return (1);
+		return (0);
 	}
 	return (is_palindrome(s + 1));
+	return (0);
 }
