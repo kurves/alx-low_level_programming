42a52a5f95e62ee86b003da47495a34bb8eabbca
kurves <kavssimply@gmail.com>
2023-10-31 10:44:11 -0700

    Program to return a pointer

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,9 +27,9 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(height * sizeof(int));
+		grid[i] = (int *)malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -37,8 +37,9 @@ int **alloc_grid(int width, int height)
 				free(grid[j]);
 			}
 			free(grid);
+			return (NULL);
 		}
-		for (j = 0; j < height; j++)
+		for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
