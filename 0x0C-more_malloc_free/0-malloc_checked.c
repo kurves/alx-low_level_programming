20369608acf9a7bcfc075a5523422ce1c6ef0565
kurves <kavssimply@gmail.com>
2023-11-02 03:56:33 -0700

    Program that allocates memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include  "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory
+ * @b: unsigned int
+ * Description: A function that allocates memory usinh malloc
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	
+	void *intb = malloc(sizeof(b));
+
+	if (intb == NULL)
+	{
+		exit(98);
+	}
+	return (intb);
+	free(intb);
+}
