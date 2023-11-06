0ac01f708257cdbdb03d04909dd15b71fb7c9e58
kurves <kavssimply@gmail.com>
2023-11-06 02:17:24 -0800

    Program that prints the file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - print file
+ * void: return  nothing
+ * description: Functions that returns file
+ * Return: integer
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,2 +1,3 @@
 ### Header file that defines a macreo named SIZE
 ### Header file that defines a macro defined PI
+### program that defines the file running the code
