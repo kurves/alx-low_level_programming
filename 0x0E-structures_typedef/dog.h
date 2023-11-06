0b251106efa464d6512f88c4aefa78b7ad2bca20
kurves <kavssimply@gmail.com>
2023-11-06 09:26:02 -0800

    fix compilation error

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,11 +3,12 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: name of dog
+ *
  * Description: struct of a dog
  */
 struct dog
 {
 	char *name;
 	float age;
-	car *owner;
-}
+	char *owner;
+};
