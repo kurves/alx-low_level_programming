41d3c08784815d802927a5e9735107839116438d
kurves <kavssimply@gmail.com>
2023-11-09 09:08:52 -0800

    Function that adds all arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - Fucntion that sums arguments
@@ -9,21 +10,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	int res = 0;
 	va_list args;
 
-	va_start(args, num);
+	va_start(args, n);
 	if (n == 0)
 	{
 		return (0);
 	}
 
 
-	for (i = 0; i < num; ++i)
+	for (i = 0; i < n; ++i)
 	{
 		res += va_arg(args, int);
 	}
 	va_end(args);
-	return (result);
+	return (res);
 }
