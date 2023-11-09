e59338543f63aa6ae6e933382f86cd4b780e26d0
kurves <kavssimply@gmail.com>
2023-11-09 11:21:09 -0800

    Betty coding style

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,27 +10,27 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        unsigned int i;
-        va_list args;
+	unsigned int i;
+	va_list args;
 	const char *str;
-        va_start(args, n);
 
-        for (i = 0; i < n; ++i)
-        {
-                str = va_arg(args, const char *);
-                if (str == NULL) 
-                {
-                        printf("(nil)");
-                }
-                else 
-                {
-                        printf("%s", str);
-                }
-                if (separator != NULL && i < n - 1) 
-                {
-                        printf("%s", separator);
-                }
-        }
-        va_end(args);
-        printf("\n");
+	va_start(args, n);
+	for (i = 0; i < n; ++i)
+	{
+		str = va_arg(args, const char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(args);
+	printf("\n");
 }
