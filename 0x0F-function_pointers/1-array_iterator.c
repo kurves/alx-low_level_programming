2a6942c42426ead92e47dba9520199f3842e3a36
kurves <kavssimply@gmail.com>
2023-11-08 22:53:49 -0800

    Fix compilation error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,16 +6,16 @@
  * array_iterator - function that iterates through an array
  * @array: integer value array
  * @size: size of array
- * @array_action: function printer
+ * @action: function printer
  * Description: function that iterates through an array of given size
  * Return: Nothing
  */
-void array_iterator(int *array, size_t size, action array_action)
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long  int i;
 
 	for (i = 0; i < size; ++i)
 	{
-		array_action(array[i]);
+		action(array[i]);
 	}
 }
