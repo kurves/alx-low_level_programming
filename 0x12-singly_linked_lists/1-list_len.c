0a436c21f69d689d63a0cfc7715c57a62ee0db02
kurves <kavssimply@gmail.com>
2023-11-16 11:43:53 -0800

    fix: fix compilation error

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,28 +4,20 @@
 #include "lists.h"
 
 /**
- * print_list - number of nodes
+ * list_len- number of nodes
  * @h: struct pointer
  * Description: singly linked list node structure
  * Return: size_t value
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-        size_t c_count;
+	size_t c_count;
 
-        c_count = 0;
-        while (h != NULL)
-        {
-                if (h->str == NULL)
-                {
-                        printf("[0] (nil)\n");
-        }
-        else
-        {
-                printf("[%lu] %s\n", c_count, h->str);
-        }
-        h = h->next;
-        c_count++;
-        }
-        return (c_count);
+	c_count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		c_count++;
+	}
+	return (c_count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,5 +18,5 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h)
+size_t list_len(const list_t *h);
 #endif
