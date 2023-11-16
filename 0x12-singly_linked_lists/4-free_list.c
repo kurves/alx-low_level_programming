f27d8e471480090b6a322adf980acdb36e9bed4c
kurves <kavssimply@gmail.com>
2023-11-16 13:25:02 -0800

    feat: function  to free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - function to free list
+ * @head: struct pointer
+ * Description: function that free a linked list
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{	
+	list_t *current = head;
+	list_t *next;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
