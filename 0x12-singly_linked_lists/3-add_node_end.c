d1dd0e64b734e66a6aa73a317bb53c68cb4792be
kurves <kavssimply@gmail.com>
2023-11-16 12:59:49 -0800

    feat: function to add node at the end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - funtion to add a node at the end
+ * @head: start of list
+ * @str: string character
+ * Description: function that adds a node to list
+ * Return: list_t value
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *new_node;
+	list_t *last = *head;
+
+        if (str == NULL)
+        {
+                return (NULL);
+        }
+        new_node = malloc(sizeof(list_t));
+
+        if (new_node == NULL)
+        {
+                return (NULL);
+        }
+        new_node->str = strdup(str);
+        if (new_node->str == NULL)
+        {
+                free(new_node);
+                return (NULL);
+        }
+        new_node->next = NULL;
+	if (*head == NULL) 
+	{
+		*head = new_node;
+	}
+	else 
+	{
+		while (last->next != NULL) 
+		{
+			last = last->next;
+		}
+		last->next = new_node;
+	}
+        return (new_node);
+}
