4c7808c1c96c40c481eaacb3afacbd840bcac5c7
kurves <kavssimply@gmail.com>
2023-11-16 23:50:13 -0800

    fix: betty coding style

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,37 +12,38 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-        list_t *new_node;
+	list_t *new_node;
 
-        if (str == NULL)
-        {
-                return (NULL);
-        }
-        new_node = malloc(sizeof(list_t));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(list_t));
 
-        if (new_node == NULL)
-        {
-                return (NULL);
-        }
-        new_node->str = strdup(str);
-        if (new_node->str == NULL)
-        {
-                free(new_node);
-                return (NULL);
-        }
-        new_node->next = NULL;
-	if (*head == NULL) 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = NULL;
+	if (*head == NULL)
 	{
 		*head = new_node;
 	}
-	else 
+	else
 	{
 		list_t *current = *head;
-		while (current->next != NULL) 
+
+		while (current->next != NULL)
 		{
 			current = current->next;
 		}
 		current->next = new_node;
 	}
-        return (new_node);
+	return (new_node);
 }
