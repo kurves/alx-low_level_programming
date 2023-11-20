63cf85ed8d8661b7287b9dce2d8406f55da2484a
kurves <kavssimply@gmail.com>
2023-11-20 03:02:46 -0800

    Function to freee alist

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_listint - function to free list
+ * @head: struct pointer
+ * Description: function that free a linked list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
