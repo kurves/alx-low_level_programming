95e3fa0615a518b206b19f20e44b3e8f773374ea
kurves <kavssimply@gmail.com>
2023-11-23 11:09:23 -0800

    fix: fix compilation error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,25 +11,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int res = 0;
-        char c_char;
+	unsigned int res = 0;
+	char c_char;
 
-        while ((c_char = *b) != '\0')
-        {
-                if (c_char == '0' || c_char == '1')
-                {
-                        res = (res << 1) | (c_char - '0');
-                }
-                else
-                {
-                        return (0);
-                }
-                b++;
-        }
-        return (res);
-
-        if (b == NULL)
-        {
-                return (0);
-        }
+	if (b == NULL)
+	{
+		return (0);
+	}
+	while ((c_char = *b) != '\0')
+	{
+		if (c_char == '0' || c_char == '1')
+		{
+			res = (res << 1) | (c_char - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		b++;
+	}
+	return (res);
 }
