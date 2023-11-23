43d91f3c3d37f2b8fdb521f4c2122569561d41ba
kurves <kavssimply@gmail.com>
2023-11-23 10:59:37 -0800

    function that converts binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to unit
+ * @b: character of strings
+ * Description: function that prints that converts binary to uint
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+        unsigned int res = 0;
+        char c_char;
+
+        while ((c_char = *b) != '\0')
+        {
+                if (c_char == '0' || c_char == '1')
+                {
+                        res = (res << 1) | (c_char - '0');
+                }
+                else
+                {
+                        return (0);
+                }
+                b++;
+        }
+        return (res);
+
+        if (b == NULL)
+        {
+                return (0);
+        }
+}
