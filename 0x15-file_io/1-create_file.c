b805044813f0c93793b29d67249c634b455171a7
kurves <kavssimply@gmail.com>
2023-11-30 03:33:52 -0800

    feat: update file permisiions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fileopen == -1)
 	{
