0ee8bc8e7d76144da9797ea8a7d96a52a2471425
kurves <kavssimply@gmail.com>
2023-11-30 03:26:44 -0800

    fix: fix compilation errror

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	fileopen = open(filename, O_WRONLY | O_APPEND);
