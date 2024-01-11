95c76cd3d716acea3536038a3e52f393f492bc0f
kurves <kavssimply@gmail.com>
2024-01-11 06:36:51 -0800

    Fix betty coding style

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,39 +18,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL) 
+	if (new_node == NULL)
 	{
-        	return NULL;
-    	}
-    	new_node->n = n;
-	
-	if (*h == NULL && idx == 0) 
+		return (NULL);
+	}
+	new_node->n = n;
+	if (*h == NULL && idx == 0)
 	{
-        	new_node->prev = NULL;
-	        new_node->next = NULL;
-        	*h = new_node;
-	        return new_node;
-    	}
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*h = new_node;
+		return (new_node);
+	}
 	current = *h;
-	while (current != NULL) 
+	while (current != NULL)
 	{
-        	if (count == idx) 
+		if (count == idx)
 		{
-	        	new_node->prev = current->prev;
-	    		new_node->next = current;
-			if (current->prev != NULL) 
+			new_node->prev = current->prev;
+			new_node->next = current;
+			if (current->prev != NULL)
 			{
 				current->prev->next = new_node;
-    			} else 
+			}
+			else
 			{
-                		*h = new_node;
-            		}
-		        current->prev = new_node;
-	    		return new_node;
-        	}
+				*h = new_node;
+			}
+			current->prev = new_node;
+			return (new_node);
+		}
 		count++;
 		current = current->next;
 	}
-    	free(new_node);
-	return NULL;
+	free(new_node);
+	return (NULL);
 }
