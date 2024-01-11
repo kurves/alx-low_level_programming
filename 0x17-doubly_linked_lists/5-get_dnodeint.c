c7ff6ce6a9dc65276bb8140c6363bcf544c5c191
kurves <kavssimply@gmail.com>
2024-01-11 01:23:46 -0800

    function to get node at index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - funtion to get node at index
+ * @head: start of list
+ * @index: index 
+ * Description: function that adds a node to list
+ * Return: list_t value
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{	
+	unsigned int count;
+
+	count = 0;
+	while (head != NULL) 
+	{
+        if (count == index) 
+	{
+            return head;
+        }
+        count++;
+        head = head->next;
+     	}
+	return NULL;
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -4,3 +4,4 @@
 ### Program that adds a node at the beginning
 ### Programt to add node at the end
 ### Program that free a double linked list
+### Program to get node at index
