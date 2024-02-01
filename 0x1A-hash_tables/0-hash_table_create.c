a7990edc383e30fd473c74fde958f05832e9f2c0
kurves <kavssimply@gmail.com>
2024-02-01 01:16:14 -0800

    function to create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,27 +13,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	hash_table = NULL;
 	unsigned long int i;
+	
+	hash_table = NULL;
 	i = 0;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
-		return NULL;
+		return (NULL);
 	}
 	hash_table->size = size;
 	for (i = 0; i < size; i++)
 	{
 		hash_table->array[i] = NULL;
-	return hash_table
 	}
-	
+	return (hash_table);
 }
-
