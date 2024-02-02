03d2303739fb58d1ed4fb0078342f0a60c7a5372
kurves <kavssimply@gmail.com>
2024-02-02 06:08:07 -0800

    function to get valu associated with key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,19 +10,23 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *current;
+
 	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		return (NULL);
 	}
-	unsigned long int index = hash_djb2((const unsigned char *)key) % ht->size;
-	hash_node_t *current = ht->array[index];
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+	current = ht->array[index];
 	
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
 			return (current->value);
-			current = current->next;
+		current = current->next;
 		}
-		return (NULL);
+	}
+	return (NULL);
 }
