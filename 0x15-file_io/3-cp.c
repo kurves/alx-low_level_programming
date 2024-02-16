6d52902e18aae47f2545fa0f1181078cefc1a296
kurves <kavssimply@gmail.com>
2024-02-16 04:36:23 -0800

    add main entry point

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,3 +51,24 @@ void copy_file_contents(const char *file_from, const char *file_to)
 		exit(100);
 	}
 }
+
+/**
+ * main- main enrty point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * description: main entry function
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		error_exit("Usage: cp file_from file_to", 97);
+	}
+	const char *file_from = argv[1];
+	const char *file_to = argv[2];
+
+	copy_file(file_from, file_to);
+	return (0);
+}
