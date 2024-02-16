d9dc05a3d55df8185af8b419fa445a9e213a742e
kurves <kavssimply@gmail.com>
2024-02-16 03:59:28 -0800

    function to copy contnt from files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,39 @@
 #include <string.h>
 
 /**
- * append_text_to_file - function to append text to file
- * @filename: fil to append text to
- * @text_content: content to be appended
- * Description: function that append file to text
+ * copy_file_contents - function to copy fike
+ * @file_from: file to copy from
+ * @file_to: file to copy to
+ * Description: function that copies content from to another
  * Return: integer value
  */
-int append_text_to_file(const char *filename, char *text_content)
+void copy_file(const char *file_from, const char *file_to) {
+    int fd_from, fd_to, bytes_read, bytes_written;
+    char buf[BUFFER_SIZE];
+
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1)
+    {
+        error_exit("Error: Can't read from file", 98);
+    }
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        error_exit("Error: Can't write to file", 99);
+    }
+    while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0) 
+    {
+        bytes_written = write(fd_to, buf, bytes_read);
+        if (bytes_written == -1)
+            error_exit("Error: Can't write to file", 99);
+    }
+    if (bytes_read == -1)
+    {
+        error_exit("Error: Can't read from file", 98);
+    }
+    if (close(fd_from) == -1 || close(fd_to) == -1)
+    {
+	    error_exit("Error: Can't close file descriptor", 100);
+
+    }
+}
