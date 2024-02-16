4e51caaef60809b73485972632743532d99e8950
kurves <kavssimply@gmail.com>
2024-02-16 04:22:24 -0800

    fix betty coding style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,39 +12,40 @@
  * Description: function that copies content from to another
  * Return: integer value
  */
-void copy_file(const char *file_from, const char *file_to) {
-    int fd_from, fd_to, bytes_read, bytes_written;
-    char buf[BUFFER_SIZE];
+void copy_file_contents(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buf[BUFFER_SIZE];
 
-    fd_from = open(file_from, O_RDONLY);
-    if (fd_from == -1)
-    {
-    	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-        exit(98);
-    }
-    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-    	 dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
-        exit(99);
-    }
-    while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0) 
-    {
-        bytes_written = write(fd_to, buf, bytes_read);
-        if (bytes_written == -1)
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file\n");
-            exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
 	}
-}
-    if (bytes_read == -1)
-    {
-    	dprintf(STDERR_FILENO, "Error: Can't read from file\n");
-        exit(98);
-    }
-    if (close(fd_from) == -1 || close(fd_to) == -1)
-    {
-	  dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
-        exit(100);
-    }
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", file_to);
+		exit(99);
+	}
+	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buf, bytes_read);
+		if (bytes_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file\n");
+		exit(98);
+	}
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close file descriptor\n");
+		exit(100);
+	}
 }
