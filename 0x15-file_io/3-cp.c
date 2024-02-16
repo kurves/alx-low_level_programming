2e3000dbf2cd9c985245216509da55353d1c277f
kurves <kavssimply@gmail.com>
2024-02-16 05:02:08 -0800

    fix compilation error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -65,3 +65,24 @@ void error_exit(const char *msg, int exit_code)
 	exit(exit_code);
 }
 
+/**
+ * main - main enrty point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * description: main entry function
+ * Return: integer
+ */
+
+int main(int ac, char **av)
+{
+        int res;
+
+        if (ac != 3)
+        {
+                dprintf(2, "Usage: %s filename text\n", av[0]);
+                exit(1);
+        }
+        res = copy_file_contents(av[1], av[2]);
+        printf("-> %i)\n", res);
+        return (0)
+}
