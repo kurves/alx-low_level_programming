ca37e7c8253186268c0283d6708373af5a75b921
kurves <kavssimply@gmail.com>
2024-05-07 08:14:01 -0700

    binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "search_algos"
+#include "search_algos.h"
 
 /**
  * binary_search - Searches for a value in a sorted array
@@ -14,32 +14,39 @@ int binary_search(int *array, size_t size, int value)
 	size_t left;
 	size_t right;
 	size_t mid;
+	size_t i;	
 	
 	left = 0;
 	right = size - 1;
-    if (array == NULL)
-        return -1;
-
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-        {
-            printf("%d", array[i]);
-            if (i < right)
-                printf(", ");
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i < right)
+			{
+				printf(", ");
+			}
         }
-        printf("\n");
-
-        if (array[mid] == value)
-            return mid;
-        else if (array[mid] < value)
+	printf("\n");
+	if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else if (array[mid] < value)
+	{
             left = mid + 1;
-        else
+	}
+	else
+	{
             right = mid - 1;
-    }
-
-    return -1; // Value not found
+	}
+	}
+	return (-1);
 }
