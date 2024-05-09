fc577f8aeaf00b84da68b308983225e26d956f5e
kurves <kavssimply@gmail.com>
2024-05-09 03:03:12 -0700

    recursion for advanced inary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -12,42 +12,30 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t low, i, mid, high;
+	int mid;
+	size_t i;
 
-	low = 0;
-	high = size - 1;
-	mid = 0;
 	if (array == NULL || size == 0)
-		return (-1);
-	while (low <= high)
 	{
-		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
-		{
-			if (i != low)
-			{
-				printf(", ");
-			}
-			printf("%d", array[i]);
-		}
-		printf("\n");
-		mid = low + (high - low) / 2;
-		if (array[mid] == value)
-		{
-			if (mid == 0 || array[mid - 1] != value)
-			{
-				return (mid);
-			}
-			high = mid;
-		}
-		else if (array[mid] < value)
-		{
-			low = mid + 1;
-		}
-		else
-		{
-			high = mid;
-		}
+		return (-1);
 	}
-	return (-1);
+	mid = size / 2;
+	printf("Searching in array: ");
+	for (i = 0; i < size; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("\n");
+	if (array[mid] == value)
+	{
+		return (mid);
+	}
+	else if (array[mid] > value)
+	{
+		return advanced_binary(array, mid, value);
+	}
+	else
+	{
+		return advanced_binary(array + mid + 1, size - mid - 1, value);
+	}
 }
